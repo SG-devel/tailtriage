292600be067c76037562785c580d2c68c8eea789
SG-devel <78623477+SG-devel@users.noreply.github.com>
2026-03-19 17:51:53 +0100

    Add shared unix-ms time utility and dedupe conversions

--- tailscope-core/src/lib.rs
+++ tailscope-core/src/lib.rs
@@ -4,6 +4,7 @@ mod collector;
 mod config;
 mod events;
 mod sink;
+mod time;
 mod timers;
 
 pub use collector::Tailscope;
@@ -12,6 +13,7 @@ pub use events::{
     InFlightSnapshot, QueueEvent, RequestEvent, Run, RunMetadata, RuntimeSnapshot, StageEvent,
 };
 pub use sink::{LocalJsonSink, RunSink, SinkError};
+pub use time::{system_time_to_unix_ms, unix_time_ms};
 pub use timers::{InflightGuard, QueueTimer, StageTimer};
 
 #[cfg(test)]
