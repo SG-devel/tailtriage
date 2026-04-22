b42be719bc8bf4c5d34771c8c49412944fa764c4
Sebastian <78623477+SG-devel@users.noreply.github.com>
2026-04-22 09:27:36 +0200

    Adjust Windows TOML regression test escaping

--- tailtriage-controller/src/lib.rs
+++ tailtriage-controller/src/lib.rs
@@ -2690,18 +2690,18 @@ mod tests {
 
     #[test]
     fn toml_parses_windows_style_escaped_output_path() {
-        let parsed: super::ControllerConfigFile = toml::from_str(
-            r#"[controller]
-
-[controller.activation]
-mode = "light"
-
-[controller.activation.sink]
-type = "local_json"
-output_path = "C:\\Users\\someone\\AppData\\Local\\Temp\\tailtriage.json"
-"#,
-        )
-        .expect("escaped Windows path should parse in TOML");
+        let config_toml = concat!(
+            "[controller]\n",
+            "\n",
+            "[controller.activation]\n",
+            "mode = \"light\"\n",
+            "\n",
+            "[controller.activation.sink]\n",
+            "type = \"local_json\"\n",
+            "output_path = \"C:\\\\Users\\\\someone\\\\AppData\\\\Local\\\\Temp\\\\tailtriage.json\"\n",
+        );
+        let parsed: super::ControllerConfigFile =
+            toml::from_str(config_toml).expect("escaped Windows path should parse in TOML");
 
         let loaded = parsed.into_loaded();
         assert_eq!(
